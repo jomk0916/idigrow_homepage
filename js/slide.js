09d5d5f994a40a9a9ff9b51452d6fae76a86f93a
jomk0916 <jomk0916@gmail.com>
2023-04-05 16:22:19 +0900

    [feature-M] 슬라이드 fade -> opacity  변경

--- js/slide.js
+++ js/slide.js
@@ -1,31 +1,45 @@
 $(document).ready(function(){
-    var i = $(".btn li.on").index();
+    var i = $(".btn li.on").index() + 1; //0
+    // console.log(i);
+    var value = 0;
+
     $(window).on("wheel",function(e){
-        console.log(e.originalEvent.deltaY);
+        // console.log(e.originalEvent.deltaY);
 
         if(e.originalEvent.deltaY > 0){
             if(i == 3){
-                
+
             }else{
                 i++;
-                
+
             }
             $("#btn li").removeClass("on");
             $("#btn li").eq(i).addClass("on");
-            $("#main section").stop().fadeOut();
-            $("#main section").eq(i).stop().fadeIn();
+            // $("#main section").stop().fadeOut();
+            $("#main section").css('opacity','0');
+            // $("#main section").eq(i).stop().fadeIn();
+            $("#main section").eq(i).css('opacity','1');
             if(i == 0){
-                console.log("1p")
+                console.log("1p");
             }
             if(i == 1){
-                console.log("2p");
+                // 1p 끝
                 $('.circle_all').addClass('js_animation');
                 $('.textBox').addClass('js_animation');
 
                 $('.circle_all').removeClass('js_animation_re');
                 $('.textBox').removeClass('js_animation_re');
-                $("#main section").stop().fadeOut(6000);
-                $("#main section").eq(i).stop().fadeIn(9000);
+
+                // $("#main section").stop().fadeOut(6000);
+                $("#main section").css('opacity','0');
+                // $("#main section").eq(i).stop().fadeIn(9000);
+                $("#main section").eq(i).css('opacity','1');
+                // 2p 시작
+
+                if($("#section2").hasClass("sec2_ani_start") === false) {
+                    $('#section2').addClass('sec2_ani_start');
+                }
+                value = 1;
             }
             if(i == 2){
                 console.log("3p");
@@ -35,20 +49,25 @@ $(document).ready(function(){
             }
         }else{
             if(i == 0){
-                
+
             }else{
                 i--;
             }
             $("#btn li").removeClass("on");
             $("#btn li").eq(i).addClass("on");
-            $("#main section").stop().fadeOut();
-            $("#main section").eq(i).stop().fadeIn();
-            if(i == 0){
-                // $('.circle_all').addClass('js_animation_re');
-                // $('.textBox').addClass('js_animation_re');
+            // $("#main section").stop().fadeOut();
+            $("#main section").css('opacity','0');
+            // $("#main section").eq(i).stop().fadeIn();
+            $("#main section").eq(i).css('opacity','1');
+            if(i == 0 && value == 1){
+                $('.circle_all').addClass('js_animation_re');
+                $('.textBox').addClass('js_animation_re');
 
-                // $('.circle_all').removeClass('js_animation');
-                // $('.textBox').removeClass('js_animation');
+                $('.circle_all').removeClass('js_animation');
+                $('.textBox').removeClass('js_animation');
+            }
+            else if (i == 1){
+
             }
         }
     });
