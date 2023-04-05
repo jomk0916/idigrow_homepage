54b2fbda80480ec67769e9f40d143e2783d5e1f9
jomk0916 <jomk0916@gmail.com>
2023-04-05 13:26:31 +0900

    [feature-M] 슬라이드 기능 작업 중

--- js/slide.js
+++ js/slide.js
@@ -5,13 +5,34 @@ $(document).ready(function(){
 
         if(e.originalEvent.deltaY > 0){
             if(i == 3){
+                
             }else{
                 i++;
+                
             }
             $("#btn li").removeClass("on");
             $("#btn li").eq(i).addClass("on");
             $("#main section").stop().fadeOut();
             $("#main section").eq(i).stop().fadeIn();
+            if(i == 0){
+                console.log("1p")
+            }
+            if(i == 1){
+                console.log("2p");
+                // $('.circle_all').addClass('js_animation');
+                // $('.textBox').addClass('js_animation');
+
+                // $('.circle_all').removeClass('js_animation_re');
+                // $('.textBox').removeClass('js_animation_re');
+                // $("#main section").stop().fadeOut(6000);
+                // $("#main section").eq(i).stop().fadeIn(9000);
+            }
+            if(i == 2){
+                console.log("3p");
+            }
+            if(i == 3){
+                console.log("4p");
+            }
         }else{
             if(i == 0){
                 
@@ -22,6 +43,20 @@ $(document).ready(function(){
             $("#btn li").eq(i).addClass("on");
             $("#main section").stop().fadeOut();
             $("#main section").eq(i).stop().fadeIn();
+            // if(i == 0){
+            //     // $('.circle_all').addClass('js_animation_re');
+            //     // $('.textBox').addClass('js_animation_re');
+
+            //     // $('.circle_all').removeClass('js_animation');
+            //     // $('.textBox').removeClass('js_animation');
+            // }
         }
+    });
+    $(".top_btn").click(function(){
+        i = $(".btn li.on").index(0);
+        $("#btn li").removeClass("on");
+        $("#btn li").eq(0).addClass("on");
+        $("#main section").stop().fadeOut();
+        $("#main section").eq(0).stop().fadeIn();
     });
 });
